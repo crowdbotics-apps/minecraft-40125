66424d4a9e4056ad968e96f88e0c0f8f92e43885
Crowdbotics <team@crowdbotics.com>
2023-04-22 20:25:24 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "minecraft-40125",
+  url: "https://minecraft-40125.botics.co",
+  api: "https://minecraft-40125.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
